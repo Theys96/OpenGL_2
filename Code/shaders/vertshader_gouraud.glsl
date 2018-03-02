68e3214d0bb402a7d2ca61348f720b0bafc8c958
JakeDavison <jake.andrew.davison@gmail.com>
2018-03-02 13:34:52 +0100

    Tweaked barely.

--- Code/shaders/vertshader_gouraud.glsl
+++ Code/shaders/vertshader_gouraud.glsl
@@ -11,7 +11,7 @@ layout (location = 1) in vec3 vertNormal_in;
  uniform mat4 modelViewTransform;
  uniform mat3 normalTransform;
  uniform mat4 projectionTransform;
- uniform vec3 lightPosition = vec3(0,1,-2);
+ uniform vec3 lightPosition = vec3(0,1,-0);
  uniform vec3 materialColour = vec3(0.3,1,0.75);
 
 // Specify the output of the vertex stage
@@ -26,7 +26,7 @@ void main()
     vec3 ambient = normalize(materialColour * 0.2);
     vec3 diffuse = max(vec3(0.0,0.0,0.0),normalize(dot(vertNormal,lightDirection) * materialColour));
     vec3 eyeDirection = vertCoordinates_in;
-    vec3 specular = pow(max(vec3(0.0,0.0,0.0), dot((2*reflect(vertNormal, lightDirection)) - lightDirection, eyeDirection)), vec3(3.0,3.0,3.0));
-    vertNormal = ambient + diffuse; + specular;
+    vec3 specular = pow(max(vec3(0.0,0.0,0.0), dot((2*reflect(vertNormal, lightDirection)) - lightDirection, eyeDirection)), vec3(5.0,5.0,5.0));
+    vertNormal = ambient + diffuse + specular;
 
 }
